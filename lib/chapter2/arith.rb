d5f2d34037c05fd4e1c845f4c56a38b158bab250
Koji NAKAMURA <kozy4324@gmail.com>
2025-05-10 11:34:11 +0900

    chapter2/arith.rb: typecheckの実装

--- lib/chapter2/arith.rb
+++ lib/chapter2/arith.rb
@@ -2,13 +2,41 @@
 
 require_relative "./tiny_rb_parser"
 
-puts TinyRbParser.parse("true").inspect
-puts TinyRbParser.parse("false").inspect
-puts TinyRbParser.parse("true ? 1 : 2").inspect
-puts TinyRbParser.parse("3").inspect
-puts TinyRbParser.parse("4 + 5").inspect
+# @rbs!
+#   type typ = { tag: "Boolean" }
+#            | { tag: "Number" }
 
-term = TinyRbParser.parse("true ? 1 : 2")
-if term.is_a? TinyRbParser::IfTerm
-  puts term.inspect
+class Checker
+  #: (TinyRbParser::Term) -> typ
+  def self.typecheck(t)
+    case
+    when t.is_a?(TinyRbParser::TrueTerm)
+      { tag: "Boolean" }
+    when t.is_a?(TinyRbParser::FalseTerm)
+      { tag: "Boolean" }
+    when t.is_a?(TinyRbParser::IfTerm)
+      condTy = typecheck(t.cond)
+      raise "boolean expected" if condTy[:tag] != "Boolean"
+      thnTy = typecheck(t.thn)
+      elsTy = typecheck(t.els)
+      raise "then and else have different types" if thnTy[:tag] != elsTy[:tag]
+      return thnTy
+    when t.is_a?(TinyRbParser::NumberTerm)
+      { tag: "Number" }
+    when t.is_a?(TinyRbParser::AddTerm)
+      leftTy = typecheck(t.left)
+      raise "number expected" if leftTy[:tag] != "Number"
+      rightTy = typecheck(t.right)
+      raise "number expected" if rightTy[:tag] != "Number"
+      { tag: "Number" }
+    else
+      raise "not implemented"
+    end
+  end
 end
+
+puts Checker.typecheck(TinyRbParser.parse("true"))
+puts Checker.typecheck(TinyRbParser.parse("false"))
+puts Checker.typecheck(TinyRbParser.parse("true ? 1 : 2"))
+puts Checker.typecheck(TinyRbParser.parse("3"))
+puts Checker.typecheck(TinyRbParser.parse("4 + 5"))

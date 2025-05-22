dd96c7b0e7aa07cb3834875e65f8d4add0204e98
Koji NAKAMURA <kozy4324@gmail.com>
2025-05-22 18:50:16 +0900

    chapter3/test_tiny_rb_parser.rb: 今書けるテストを追加

--- lib/chapter3/test_tiny_rb_parser.rb
+++ lib/chapter3/test_tiny_rb_parser.rb
@@ -15,3 +15,6 @@ end
 assert_parse_result('true', Chapter3::TinyRbParser::TrueTerm)
 assert_parse_result('false', Chapter3::TinyRbParser::FalseTerm)
 assert_parse_result('true ? 1 : 2', Chapter3::TinyRbParser::IfTerm)
+assert_parse_result('1', Chapter3::TinyRbParser::NumberTerm)
+assert_parse_result('1 + 2', Chapter3::TinyRbParser::AddTerm)
+assert_parse_result('-> { 1 }', Chapter3::TinyRbParser::FuncTerm)
